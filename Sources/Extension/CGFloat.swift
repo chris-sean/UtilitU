238e35e5295c2bfecd25e5ea690aa5f86a22983b
Felix <SimpleFelix@users.noreply.github.com>
2019-12-13 19:14:26 +0800

    c

---
+++ Sources/Extension/CGFloat.swift
@@ -0,0 +1,14 @@
+//
+//  File.swift
+//  
+//
+//  Created by 薛菲 on 2019/12/13.
+//
+
+import UIKit
+
+extension CGFloat {
+    static var OnePixel: CGFloat {
+        return 1.0 / UIScreen.main.scale
+    }
+}
